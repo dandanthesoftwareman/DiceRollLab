bcad58be7fdd5836b5168b8db2cbb38232a1938b
Dan Schuler <104746354+dandanthesoftwareman@users.noreply.github.com>
2022-07-01 01:39:40 -0400

    Post-gig commit. Few modifications, new variables

--- DiceRollerLab/Program.cs
+++ DiceRollerLab/Program.cs
@@ -2,17 +2,21 @@
 int dieSides = 0;
 int die1 = 0;
 int die2 = 0;
+int roll = 1;
+int total = 0;
 bool rollDice = true;
 bool setupDice = true;
+
+//Greeting//
 Console.WriteLine("Welcome to the dice roller");
 
+//Set die sides, user-proof input. Loops until user enters valid input
 while (setupDice)
 {
     try
     {
         Console.WriteLine("How many sides should each die have?");
         dieSides = int.Parse(Console.ReadLine());
-
 
         if (dieSides <= 0)
         {
@@ -34,16 +38,22 @@ while (rollDice)
 {
     die1 = rollDie(dieSides);
     die2 = rollDie(dieSides);
+    total = die1 + die2;
+
+    //Starts the roll for 6 sided dice. loops until user enters "n" to stop program
+    //uses getCombos method to return pairings
     if (dieSides == 6)
     {
-        Console.WriteLine($"You rolled a {die1} and a {die2} (Total {die1 + die2})");
-        Console.WriteLine(getCombos(die1, die2));
+        Console.WriteLine($"Roll {roll}:");
+        Console.WriteLine($"You rolled a {die1} and a {die2} (Total {total})");
+        Console.WriteLine(getCombos(die1, die2) + " " + diceTotals(total));
         while (true)
         {
             Console.WriteLine("Roll Again? y/n");
             string choice = Console.ReadLine();
             if (choice == "y")
             {
+                roll++;
                 break;
             }
             else if (choice == "n")
@@ -59,7 +69,10 @@ while (rollDice)
     }
     else
     {
-        Console.WriteLine($"You rolled a {die1} and a {die2} (Total {die1 + die2})");
+        //Starts the dice roll for user specified dice. 
+        //uses userDice method to return pairings
+        Console.WriteLine($"Roll {roll}:");
+        Console.WriteLine(getCombos(die1, die2) + " " + diceTotals(total));
         Console.WriteLine(userDice(die1, die2));
         while (true)
         {
@@ -67,6 +80,7 @@ while (rollDice)
             string choice = Console.ReadLine();
             if (choice == "y")
             {
+                roll++;
                 break;
             }
             else if (choice == "n")
@@ -103,7 +117,15 @@ static string getCombos(int x, int y)
     {
         return "Box Cars!";
     }
-    if (x + y == 3 || x + y == 12 || x + y == 2)
+    else
+    {
+        return " ";
+    }
+}
+
+static string diceTotals(int x)
+{
+    if (x == 3 || x == 12 || x == 2)
     {
         return "Craps!";
     }
@@ -117,11 +139,19 @@ static string userDice(int x, int y)
 {
     if (x == y)
     {
-        return "Mathcing Baby";
+        return "Hey! Doubles!";
     }
     if (x + y == 69)
     {
         return "nice";
+    }
+    if (x + y == 314 || (x == 3 && y == 14) || (y == 3 && x == 14))
+    {
+        return "pi";
+    } 
+    if (x == 2 && y == 2)
+    {
+        return "hop on the two-two train!";
     }
     else
     {
